22f666107dff5fb3d9040d10b613bb4a178ef425
PaolaFU <paola_sfu@hotmail.com>
2017-12-31 02:49:02 -0500

    Agregando archivo select-food.html y data.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
-   
-    //$("#logo-food").fadeToggle();
-    $("#title-welcome").fadeToggle(1000);
-    //$("#div1").fadeOut();
-    //$("#div2").fadeOut("slow");
-    //$("#div3").fadeOut(3000);
-    
-   
-   
-    setTimeout(function() {
-      window.location.href = 'views/select-restaurant.html';
-    }, 3000);
-  });
+  /* animacion el titulo Bienvenido */
+  $('#title-welcome').fadeToggle(3500);
+  /* Despues de unos segundo pasa a la sgt. vista */
+  setTimeout(function() {
+    window.location.href = 'views/select-restaurant.html';
+  }, 3000);
+});
---
+++ js/data.js
@@ -0,0 +1,91 @@
+var comeRico = [
+  {
+    name: 'Mi Barunto',
+    address: ['Jiron Sebastian Barranca 985, La Victoria'],
+    website: 'https://www.mibarrunto.com/',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Criolla', 'Pescados y mariscos'],
+    Raiting: ['4.6/5'], 
+  },
+
+  {
+    name: 'La red',
+    address: ['La Mar 391 Santa Cruz Miraflores'],
+    website: 'https://www.facebook.com/restaurante.cebicheria.lared',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Sushi', 'Pescados y mariscos'],
+    Raiting: ['3.8/5'], 
+  },
+
+  {
+    name: 'La Romana',
+    address: ['Av Antonio José de Sucre 347, Pueblo Libre 15084'],
+    website: 'http://www.laromana.com.pe/laromana.htm',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Pastas y pizzas'],
+    Raiting: ['4.1/5'], 
+  },
+
+  {
+    name: 'Hikari Pollos',
+    address: ['Av. La Mar 2339, San Miguel 15088'],
+    website: 'http://hikari.pe/',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Criolla', 'Pastas y pizzas'],
+    Raiting: ['4.1/5'], 
+  },
+
+  {
+    name: 'Veggie Peru',
+    address: ['Jr. Colina 112, Barranco'],
+    website: 'https://www.veggiepizza.pe/',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Vegana'],
+    Raiting: ['3.5/5'], 
+  },
+  
+  {
+    name: 'Roll Star Sushi',
+    address: ['Jr. Colina 112, Barranco'],
+    website: 'https://www.veggiepizza.pe/',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Sushi'],
+    Raiting: ['3.5/5'], 
+  },
+
+  {
+    name: 'Pizzeria La Linterna',
+    address: ['Calle Los Libertadores 311, San Isidro 15073'],
+    website: 'http://www.pizzerialalinterna.com/',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Pastas y pizzas'],
+    Raiting: ['4.0/5'], 
+  },
+
+  {
+    name: 'Panchita - Miraflores',
+    address: ['Calle 2 de Mayo 298, Miraflores 15074'],
+    website: 'http://www.pizzerialalinterna.com/',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Criolla'],
+    Raiting: ['4.6/5'], 
+  },
+
+  {
+    name: 'Yami Vegan',
+    address: ['Jirón Domingo Casanova 254, Lince LIMA 14'],
+    website: 'https://www.facebook.com/yamiveg/',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Vegana'],
+    Raiting: ['4.5/5'], 
+  },
+
+  {
+    name: 'Ktana cocina japonesa',
+    address: ['Av. Gregorio Escobedo 803, Jesús María 15076'],
+    website: 'http://www.ktanarestaurante.com/',
+    photo: '<img src=\'assets/img/runaway-sushi.jpg\' alt=\'Runaway Sushi\' class=\'mouseover\'>',
+    filters: ['Sushi'],
+    Raiting: ['4.7/5'], 
+  },
+];
