5a0374c61f35e5e202ffae47cf205b3718c4c7de
PaolaFU <paola_sfu@hotmail.com>
2017-12-30 20:06:11 -0500

    Agregando vista splash, estilos e imagenes

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+   
+    //$("#logo-food").fadeToggle();
+    $("#title-welcome").fadeToggle(1000);
+    //$("#div1").fadeOut();
+    //$("#div2").fadeOut("slow");
+    //$("#div3").fadeOut(3000);
+    
+   
+   
+    setTimeout(function() {
+      window.location.href = 'views/select-restaurant.html';
+    }, 3000);
+  });
